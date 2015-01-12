9700265b4ce22f22506626c8364a40b900bfa70c
Steven Marr <stevenmarr@me.com>
2015-01-11 23:33:09 -0700

    Add code from http://stackoverflow.com/questions/81451/upload-files-in-google-app-engine and https://cloud.google.com/appengine/docs/python/tools/webapp/blobstorehandlers

--- main.py
+++ main.py
@@ -15,6 +15,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import urllib
+from dropbox import *
+from google.appengine.ext import blobstore
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+import secrets
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        upload_url = blobstore.create_upload_url('/upload')
+        self.response.out.write('<html><body>')
+        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+        self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit" name="submit" value="Submit"> </form></body></html>""")
+
+        for b in blobstore.BlobInfo.all():
+            #insert call to drop box to begin transfer process
+            self.response.out.write('<li><a href="/serve/%s' % str(b.key()) + '">' + str(b.filename) + '</a>')
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        upload_files = self.get_uploads('file')
+        blob_info = upload_files[0]
+        self.redirect('/')
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, blob_key):
+        blob_key = str(urllib.unquote(blob_key))
+        if not blobstore.get(blob_key):
+            self.error(404)
+        else:
+            self.send_blob(blobstore.BlobInfo.get(blob_key), save_as=True)
+
+app = webapp.WSGIApplication(
+          [('/', MainHandler),
+           ('/upload', UploadHandler),
+           ('/serve/([^/]+)?', ServeHandler),
+          ], debug=True)
+
+
+
+
+"""
+
 import webapp2
 import setuptools
 import urllib3
@@ -65,4 +111,4 @@ def getOauthToken():
 
 
 app = webapp2.WSGIApplication([('/', MainPage),('/init', Auth),('/upload', UploadPreso),('/authorize', Auth),('/capture', CaptureAuth)],
-                                debug=True)
+                                debug=True)"""
