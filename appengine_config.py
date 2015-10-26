d94224ce56582d020950f1abea3ec7e261bb64af
stevenmarr <stevenmarr@me.com>
2015-10-26 14:41:26 -0600

    Fix bug

--- appengine_config.py
+++ appengine_config.py
@@ -1,8 +1,5 @@
 from google.appengine.ext import vendor
 
 # Add any libraries installed in the "lib" folder.
-vendor.add('dropbox')
-vendor.add('dateutil')
-vendor.add('wtforms')
-vendor.add('urllib3')
+vendor.add('lib')
 
