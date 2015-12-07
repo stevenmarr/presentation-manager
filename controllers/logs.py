24c4d2a788d07237af83d64304d05321a9844db0
stevenmarr <stevenmarr@me.com>
2015-12-06 18:56:09 -0700

    Refactor for cleaner code

--- controllers/logs.py
+++ controllers/logs.py
@@ -1,17 +1,25 @@
-import logging
-from google.appengine.ext import db
 
 from mainh import BaseHandler
 from helpers import user_required
-from models import data_cache, AppEventData
+from models import AppEventData
 
+class LogsHandler(BaseHandler):
+    @user_required
+    def get(self):
+        self.render_response('logs.html',
+            user_events = AppEventData.query('user', self.module),
+            session_events = AppEventData.query('session', self.module),
+            file_events =  AppEventData.query('events', self.module))
 
+# old handler, refactored 20151206
+"""
 class LogsHandler(BaseHandler):
     @user_required
     def get(self):
         user_events = data_cache.get('%s-user_events'% self.module)
         if user_events == None:
             user_events = db.GqlQuery("SELECT * FROM AppEventData WHERE event_type = 'user' and module = '%s' ORDER BY time_stamp DESC LIMIT 50"% self.module)
+            
             logging.info('AppEventData DB Query')
             data_cache.set('%s-user_events'% self.module, user_events)
         session_events = data_cache.get('%s-session_events'% self.module)
@@ -28,3 +36,4 @@ class LogsHandler(BaseHandler):
                                 user_events =           user_events,
                                 session_events =        session_events,
                                 file_events =           file_events)
+"""
