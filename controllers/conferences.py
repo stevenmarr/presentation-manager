73d2e0a80874f96a1e9de1bbf64fdbabbe0a332f
stevenmarr <stevenmarr@me.com>
2015-12-25 10:10:35 -0700

    Update conference_data to call ConferenceData class for conferences, new logging, remove memcache

--- controllers/conferences.py
+++ controllers/conferences.py
@@ -3,26 +3,29 @@
 import logging
 
 from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext import db, ndb
 from google.appengine.ext import blobstore
 from dateutil.parser import parse
 from webapp2 import uri_for
 
 from mainh import BaseHandler
 from helpers import  admin_required # user_required
-from forms import ConferenceForm
-#from models import SessionData, AppEventData, User, data_cache
+from models.forms import ConferenceForm, ConferenceDataForm
+from models.dbmodels import ConferenceData
+
+log = logging.getLogger(__name__)
 
 class ManageConferenceHandler(BaseHandler):
     @admin_required
     def get(self):
-        conference_data = self.get_conference_data()
-        form = ConferenceForm(obj = conference_data)
+        conference_data = ConferenceData.get_config()
+        form = ConferenceDataForm(obj = conference_data)
         form.start.data = ('%s'% conference_data.start_date)#.date()
         form.end.data = ('%s'% conference_data.end_date)#.date()
         return self.render_response('edit_conference.html', form=form)
 
     def post(self):
-        conference_data = self.get_conference_data()
+        conference_data = ConferenceData.get_config()
         form = ConferenceForm(self.request.POST, obj = conference_data)
         if not form.validate():
             return self.render_response('edit_conference.html',
@@ -33,8 +36,8 @@ class ManageConferenceHandler(BaseHandler):
         conference_data.start_date = parse('%s'% form.start.data).date()
         conference_data.end_date = parse('%s'% form.end.data).date()
         #TODO: compare values, make sure they are in chronological order.
+        #conference_data.parent = ndb.Key('conference', selfmodule)
         conference_data.save()
-        data_cache.set('%s-conference_data'% self.module, None)
         return self.redirect(uri_for('conference_data'))
 
 class RenderConferenceUploadDataHandler(blobstore_handlers.BlobstoreUploadHandler, BaseHandler):
@@ -102,8 +105,7 @@ class CommitConferenceUploadData(BaseHandler):
                                             lastname =      lastname,
                                             verified =      False)
             if created:
-                AppEventData(event = email, event_type='user', transaction='CREATE', user = self.user.email).put()
-                data_cache.set('events', None)
+                logging.info("User Created via CSV: %s" %(user_id))
             if created and email_user == 'on':
 
                 url = self.uri_for('activate', _full=True)
@@ -124,10 +126,9 @@ class CommitConferenceUploadData(BaseHandler):
                                     user_id =       email,
                                     name =  name,
                                     room =  room)
-            AppEventData(event = name, event_type='session', transaction='CREATE', user = self.user.email).put()
-            data_cache.set('events', None)
             session.put()
-            data_cache.set('sessions', None)
+            logging.info("Session Created via CSV: %s" %(name))
 
         session_data_info.delete()
-        self.redirect(ur_for('sessions'))
+        self.redirect(uri_for('sessions'))
+
