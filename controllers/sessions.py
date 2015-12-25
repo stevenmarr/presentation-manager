5a7f3d60166cd1ac846cb2172a2e3995dca2f5d6
stevenmarr <stevenmarr@me.com>
2015-12-25 10:08:51 -0700

    Update handlers based on changing sessions to ndb model, combine UpdateHandler (GET and POST), Remove EditHandler, add logging

--- controllers/sessions.py
+++ controllers/sessions.py
@@ -1,15 +1,19 @@
 import logging
+import pdb
+import sys
 
 from google.appengine.ext.webapp import blobstore_handlers
-from google.appengine.ext import db
+from google.appengine.ext.db import BadKeyError, Error
+from google.appengine.ext import db, ndb
 from dateutil.parser import parse
 from webapp2 import uri_for
 
 from mainh import BaseHandler
 from helpers import user_required, admin_required
-from forms import SessionForm
-from models import SessionData, AppEventData, User, data_cache
+from models.forms import SessionForm
+from models.dbmodels import SessionData, User
 
+log = logging.getLogger(__name__)
 
 class AddSessionHandler(blobstore_handlers.BlobstoreUploadHandler,  BaseHandler):
     @admin_required
@@ -20,51 +24,47 @@ class AddSessionHandler(blobstore_handlers.BlobstoreUploadHandler,  BaseHandler)
             return self.render_response('manage_sessions.html',
                                         failed = True,
                                         message = 'Invalid add session submission',
-                                        sessions=   self.get_sessions(),
+                                        sessions=   SessionData.get_sessions(),
                                         form =      form)
         user_id = form.users.data
-        query = ndb.gql("SELECT * FROM User WHERE email = '%s'"% user_id)
-        presenter = query.get()
-        session = SessionData(Parent = presenter)
+        presenter=User.query_user(user_id)
+        session = SessionData(parent=ndb.Key('sessions', self.module))
+        
         form.populate_obj(session)
-        session.date = str(parse('%s'% form.date.data).date())
-        session.time = str(parse('%s'% form.time.data).time().isoformat())[0:5]
-        session.dotw = parse('%s'% form.date.data).date().strftime("%A")
         session.user_id = user_id
         session.presenter = self.get_users(user_id)
-        session.save()
-
-        #time.sleep(.25)
-        return self.redirect(uri_for('sessions'))
-                                #,
-                                ##success = True,
-                                #message = ('%s - session created successfully' %session.name),
-                                #sessions =  self.get_sessions(),
-                                #form =      form)
-
+        session.put()
+        logging.info("Session Created: %s" %(session.name))
+        self.redirect(uri_for('sessions'))
+    
 
 class SessionsHandler(BaseHandler):
     @user_required
     def get(self):
-        sessions = self.get_sessions()
+        sessions = SessionData.get_sessions()
+        #form = SessionForm()
         form = SessionForm()
         form.users.choices = self.get_users_tuple()
+        
         #dates = SessionData.all().filter('module =', self.module).group('date').get()
-        result = db.GqlQuery("SELECT date, dotw FROM SessionData WHERE module = '%s' ORDER BY date DESC"% self.module)
+        # TODO: Refactor this mess, this should be a class method of SessionData that returns all days of the week there are session for
+        days_of_the_week = SessionData.get_days_of_the_week()
+        #result = ndb.gql("SELECT date, dotw FROM SessionData WHERE module = '%s' ORDER BY date DESC"% self.module)
         # build a list of days of the week do sort dates by
-        dates = {}
-        for date in result:
-            if date.date in dates: pass
-            else: dates[date.date]=date.dotw
-        return self.render_response("manage_sessions.html",
-                                sessions =  sessions,
-                                form =      form,
-                                dates =     dates)
+        #dates = {}
+        #for date in result:
+        #    if date.date in dates: pass
+        #    else: dates[date.date]=date.dotw
+        return self.render_response("manage_sessions.html", 
+                                dates=days_of_the_week, 
+                                sessions = sessions,
+                                form = form)
 
 class SessionByDateHandler(BaseHandler):
     @user_required
     def get(self, date):
-        sessions = db.GqlQuery("SELECT * FROM SessionData WHERE date = '%s'"% date)
+        sessions = SessionData.get_sessions_by_date(date)
+        #sessions = db.GqlQuery("SELECT * FROM SessionData WHERE date = '%s'"% date)
         weekdays = {7:'Sunday',1:'Monday',2:'Tuesday',3:'Wednesday',4:'Thursday',5:'Friday',6:'Saturday'}
 
         dotw = weekdays[parse(date).date().isoweekday()]
@@ -73,22 +73,18 @@ class SessionByDateHandler(BaseHandler):
                                         dotw = dotw)
 
 
-class EditSessionHandler(BaseHandler):
-    @user_required
-    def post(self):
-        key = self.request.get('session_key')
+class UpdateSessionHandler(blobstore_handlers.BlobstoreUploadHandler, BaseHandler):
+    @admin_required
+    def get(self,key):
         try:
-            session = SessionData.get(key)
+            key = ndb.Key(urlsafe=key)
+            session = key.get()
         except:
+            logging.error("Unexpected error: %s"% sys.exc_info()[0])
             return self.redirect(uri_for('sessions'))
         user = User.query(User.email == session.user_id).get()
         form = SessionForm(obj = session)
         form.users.choices = self.get_users_tuple()
-        if form.date.data:
-            form.date.data = parse('%s'% session.date).date()
-        if form.time.data:
-            form.time.data = parse('%s'% session.time).time()
-        
         if user:
             form.users.choices.insert(0, (session.user_id, user.lastname+', '+user.firstname))
             return self.render_response("edit_session.html",
@@ -100,16 +96,12 @@ class EditSessionHandler(BaseHandler):
                                 message = 'That presenter no longer exists in the database, please choose a new presenter',
                                 form = form,
                                 key =   key)
-
-
-class UpdateSessionHandler(blobstore_handlers.BlobstoreUploadHandler, BaseHandler):
-    @admin_required
-    def post(self):
-        key = self.request.get('key')
+    def post(self, key):
         try:
-            session = SessionData.get(key)
+            key = ndb.Key(urlsafe=key)
+            session = key.get()
         except:
-            logging.error("Invalid key from post data %s" % key)
+            logging.error("Unexpected error: %s"% sys.exc_info()[0])
             return self.redirect(uri_for('sessions'))
         form = SessionForm(self.request.POST, obj=session)
         form.users.choices = self.get_users_tuple()
@@ -121,35 +113,30 @@ class UpdateSessionHandler(blobstore_handlers.BlobstoreUploadHandler, BaseHandle
                                         form=form,
                                         key=key)
         form.populate_obj(session)
-        session.date = str(parse('%s'% form.date.data).date())
-        session.time = str(parse('%s'% form.time.data).time().isoformat())[0:5]
-        session.dotw = parse('%s'% form.date.data).date().strftime("%A")
         session.user_id = form.users.data
-        session.save()
-        data_cache.set('%s-sessions'% self.module, None)
-        #time.sleep(.25)
+        session.put()
+        logging.info("Session Updated: %s" %(session.name))
         form = SessionForm()
         form.users.choices = self.get_users_tuple()
-        return self 
-        #self.render_response('manage_sessions.html',
-        #                            success = True,
-        #                            message = ('%s - session edited successfully' %session.name),
-        #                            sessions =  self.get_sessions(),
-        #                            form =      form)
+        self.redirect(uri_for('sessions'))
 
 
 class DeleteSessionHandler(BaseHandler):
     @admin_required
     def post(self):
-        key = self.request.get('session_key')
-        try: session = SessionData.get(key)
-        except: return self.redirect(uri_for('sessions'))
+        try:
+            session_key = ndb.Key(urlsafe=self.request.get('session_key'))
+            session = session_key.get()
+        except:
+            logging.error("Unexpected error: %s"% sys.exc_info()[0])
+            return self.redirect(uri_for('sessions'))
         if session:#key = session.blob_store_key
             if session.blob_store_key != None:
                 session.blob_store_key.delete()
-            AppEventData(event = session.name, event_type='session', transaction='DEL', user = self.user.email).put()
-            data_cache.set('events', None)
-            session.delete()
-            #time.sleep(.25)
-        self.redirect('/admin/manage_sessions')
+            logging.info('self.user %s'% self.user)
+            AppEventData(event = session.name, event_type='session', transaction='DEL', user = self.user_info['email']).put()
+            logging.info("Session Deleted: %s" %(session.name))
+            session_key.delete()
+            
+        return self.redirect(uri_for('sessions'))
 
